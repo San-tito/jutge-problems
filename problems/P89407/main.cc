2096f22978ad4f6c58800f7ba85eabde7d8c56fc
ruiznaomy13 <ruiznaomy13@gmail.com>
2026-01-07 21:10:28 +0100

    stable

--- problems/P89407/main.cc
+++ problems/P89407/main.cc
@@ -2,6 +2,23 @@
 
 int main(void)
 {
-    std::cout << "Hello, Santito!\n";
+    int x, y;
+
+    while (std::cin >> x >> y)
+    {
+        std::cout << "solutions for " << x << " and " << y << std::endl;
+        bool none = true;
+        for (int b = 2; b <= 16; ++b)
+        {
+            if (is_stable_product(x, y, b))
+            {
+                none = false;
+                std::cout << "(base " << b << ')';
+                std::cout << std::endl;
+            }
+        }
+        if (none)
+            std::cout << "none of them" << std::endl;
+    }
     return 0;
 }
