604276da028e21e3f478af94d39754745ba33fd3
ruiznaomy13 <ruiznaomy13@gmail.com>
2026-01-11 11:25:20 +0100

    a

--- problems/P45829/main.cc
+++ problems/P45829/main.cc
@@ -1,7 +1,39 @@
+#include <cassert>
 #include <iostream>
+#include <vector>
+
+typedef std::vector<std::vector<int>> matrix;
+
+matrix read_matrix(int n, int m)
+{
+    matrix mat(n, std::vector<int>(m));
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; ++j)
+            std::cin >> mat[i][j];
+    }
+
+    return mat;
+}
 
 int main(void)
 {
-    std::cout << "Hello, Santito!\n";
+    int n, m, count;
+
+    while (std::cin >> n >> m)
+    {
+        count = 0;
+        matrix mat = read_matrix(n, m);
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                if (mat[i][j] and ((i + 1 == n) or mat[i + 1][j] == 0) and ((j + 1 == m) or mat[i][j + 1] == 0))
+                    ++count;
+            }
+        }
+        std::cout << count << std::endl;
+    }
     return 0;
 }
--- problems/X17276/main.cc
+++ problems/X17276/main.cc
@@ -1,7 +1,65 @@
+#include <cassert>
 #include <iostream>
+#include <vector>
+
+typedef std::vector<std::vector<int>> matrix;
+
+struct pos
+{
+    int x;
+    int y;
+};
+
+/*
+ * @pre n,m > 0
+ * @post read matrix from standard input and return it
+ */
+matrix read_matrix(int n, int m)
+{
+    assert(n > 0);
+    assert(m > 0);
+
+    matrix mat(n, std::vector<int>(m));
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+            std::cin >> mat[i][j];
+    }
+
+    return mat;
+}
+
+/*
+ * @pre mat is not empty, p is a valid mat position
+ * @post return true if  p to
+ */
+bool check(const matrix &mat, pos p, int dx, int dy)
+{
+    const int n = mat.size();
+    assert(p.x < n);
+
+    const int m = mat[0].size();
+    assert(p.y < m);
+
+    for(int i = 0; p.x + i < n and p.y + i < m; ++i)
+    {
+	std::cout << mat[p.x + i][p.y + i] << " ";
+    }
+
+    return true;
+}
 
 int main(void)
 {
-    std::cout << "Hello, Santito!\n";
+    int n, m;
+    while (std::cin >> n >> m)
+    {
+        matrix mat = read_matrix(n, m);
+        pos    p;
+        std::cin >> p.x >> p.y;
+        bool is = check(mat, p, 1, 1);
+        std::cout << is << std::endl;
+    }
     return 0;
 }
