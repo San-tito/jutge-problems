d5d9943e29b20ff372afca9447daac328d76bc61
ruiznaomy13 <ruiznaomy13@gmail.com>
2026-01-12 15:59:27 +0100

    algorithm:

--- problems/P81966/main.cc
+++ problems/P81966/main.cc
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <vector>
+
+int position(double x, const std::vector<double> &v, int left, int right)
+{
+    if (left > right)
+        return -1;
+    int mid = left + (right - left) / 2;
+    if (v[mid] == x)
+        return mid;
+    else if (v[mid] < x)
+        return position(x, v, mid + 1, right);
+    else
+        return position(x, v, left, mid - 1);
+}
 
 int main(void)
 {
-    std::cout << "Hello, Santito!\n";
+    int n;
+    while (std::cin >> n)
+    {
+        std::vector<double> V(n);
+        for (int i = 0; i < n; ++i)
+            std::cin >> V[i];
+        int t;
+        std::cin >> t;
+        while (t--)
+        {
+            double x;
+            int    left, right;
+            std::cin >> x >> left >> right;
+            std::cout << position(x, V, left, right) << std::endl;
+        }
+    }
     return 0;
 }
